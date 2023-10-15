51bb73c716b455a3b9dfd1ec91e21e2dd56c22a6
pjy <wodus331@gmail.com>
2023-10-16 00:06:51 +0900

    https://www.youtube.com/watch?v=AaiijESQH5o 2:49:47

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import PriceTag from "@/lib/PriceTag";
 import {Metadata} from "next";
 import {cache} from "react"
+import AddToCartButton from "@/app/products/[id]/AddToCartButton";
 
 interface ProductPageProps {
   params: {
@@ -48,6 +49,7 @@ export default async function ProductPage({params: {id}}: ProductPageProps) {
         <h1 className='text-5xl font-bold'>{product.name}</h1>
         <PriceTag price={product.price} className='mt-4'/>
         <p className='py-6'>{product.description}</p>
+        <AddToCartButton productId={product.id}/>
       </div>
     </div>
   )
